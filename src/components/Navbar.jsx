3757f850090316b055a56ec31a4ab401045566dd
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-18 23:45:37 +0100

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Badge } from "@material-ui/core";
+import styled from 'styled-components'
+import { BsBasket3} from 'react-icons/bs'
 
-import styled from 'styled-components';
 
 
+import {AiOutlineSearch} from 'react-icons/ai'
 const Container =styled.div`
 height:60px;
 `
@@ -12,17 +15,39 @@ display:flex;
 `
 const Left = styled.div `
 flex:1;
+display:flex;
+aligin-item:center;
+
 `
 const Langue = styled.div`
 font-size:14;
 cursor: pointer;
 `
+const SearcContainer = styled.div`
+border: 0.5px solid lightgray;
+display:flex;
+aligin-item:center;
+margin-left:25px;
+padding:5px;
+`
 const Center = styled.div `
 flex:1;
+text-aligin:center;
+`
+const Input =styled.input`
+border:none;
+`
+const Logo = styled.h1`
+font-weight:bold;
 `
 
 const Right = styled.div `
 flex:1;
+`
+const MenuItem = styled.div`
+font-size:14px;
+cursor:pointer;
+
 `
 
 
@@ -39,12 +64,25 @@ const Navbar = () => {
                         EN
                         </Langue>  
                         <SearcContainer>
-                            input 
-                            icon
+                            <Input />
+                    <AiOutlineSearch />
                         </SearcContainer>
                             </Left>
-                <Center>CENTER</Center>
-                <Right>RIGT</Right>
+                <Center><Logo>Lama</Logo></Center>
+                <Right>
+                    
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>sign in</MenuItem>
+                    <MenuItem>
+                    <Badge badgeContent={4} color="secondary">
+
+                    <BsBasket3/>
+                    </Badge>
+        
+            
+                    </MenuItem>
+                    
+                    RIGT</Right>
             
             </Wrapper>
         
